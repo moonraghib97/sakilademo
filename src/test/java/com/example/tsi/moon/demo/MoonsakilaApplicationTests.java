16c4df9cb244d56c32f3d93a74d636eae697c53b
moonr <moonraghib97@gmail.com>
2022-02-08 15:28:25 +0000

    first commit

---
+++ src/test/java/com/example/tsi/moon/demo/MoonsakilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsi.moon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonsakilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
