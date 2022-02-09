515c0247687f5eb4a09452996a53a0761e83a92a
moonr <moonraghib97@gmail.com>
2022-02-09 09:36:00 +0000

    local host now workinh

--- src/main/java/com/example/tsi/moon/demo/MoonsakilaApplication.java
+++ src/main/java/com/example/tsi/moon/demo/MoonsakilaApplication.java
@@ -8,23 +8,27 @@ import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
 @RestController
 @RequestMapping("/Home")
+
 public class MoonsakilaApplication {
 
 	@Autowired
 	private LanguageRepository languageRepository;
 
+
+
 	public MoonsakilaApplication(LanguageRepository languageRepository){
-		this.languageRepository = languageRepository;
+
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoonsakilaApplication.class, args);
 
 	}
-
 	@GetMapping("/AllLanguages")
 	public @ResponseBody
-	Iterable<Language> getAllLanguages(){
+	Iterable <Language> getAllLanguages(){
 		return languageRepository.findAll();
 	}
+
+
 }
